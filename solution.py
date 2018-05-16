0ad8045d74c204e855707acb20739a939f0e83c8
Tanveer Singh Sapra <tandavsingh@gmail.com>
2018-05-16 14:52:02 +0530

    Update solution.py

--- solution.py
+++ solution.py
@@ -2,33 +2,36 @@
 from utils import *
 
 
+def cross(a, b):
+    return [s+t for s in a for t in b]
+
+rows = 'ABCDEFGHI'
+cols = '123456789'
+revcol = cols[::-1]
+boxes = cross(rows, cols)
+
 row_units = [cross(r, cols) for r in rows]
 column_units = [cross(rows, c) for c in cols]
 square_units = [cross(rs, cs) for rs in ('ABC','DEF','GHI') for cs in ('123','456','789')]
-unitlist = row_units + column_units + square_units
+d1 = [[rows[i]+cols[i] for i in range(len(rows))]]
+d2 = [[rows[i]+revcol[i] for i in range(len(rows))]]
 
-# TODO: Update the unit list to add the new diagonal units
-unitlist = unitlist
+unitlist = row_units + column_units + square_units + d1 + d2
 
-
-# Must be called after all units (including diagonals) are added to the unitlist
-units = extract_units(unitlist, boxes)
-peers = extract_peers(units, boxes)
+units = dict((s, [u for u in unitlist if s in u]) for s in boxes)
+peers = dict((s, set(sum(units[s],[]))-set([s])) for s in boxes)
 
 
 def naked_twins(values):
     """Eliminate values using the naked twins strategy.
-
     Parameters
     ----------
     values(dict)
         a dictionary of the form {'box_name': '123456789', ...}
-
     Returns
     -------
     dict
         The values dictionary with the naked twins eliminated from peers
-
     Notes
     -----
     Your solution can either process all pairs of naked twins from the input once,
@@ -38,110 +41,122 @@ def naked_twins(values):
     of naked twins from the original input. (For example, if you start processing
     pairs of twins and eliminate another pair of twins before the second pair
     is processed then your code will fail the PA test suite.)
-
     The first convention is preferred for consistency with the other strategies,
     and because it is simpler (since the reduce_puzzle function already calls this
     strategy repeatedly).
     """
     # TODO: Implement this function!
-    raise NotImplementedError
+    #finding all bi-valued boxes
+    bi_boxes=[box for box in values.keys() if len(values[box]) == 2]
 
+    #finding box pair having same value
+    nktwins = [[box1,box2] for box1 in bi_boxes for box2 in peers[box1] if values[box1] == values[box2]]
+
+    for n in range(len(nktwins)):
+        box1 = nktwins[n][0]
+        box2 = nktwins[n][1]
+        #find common peers (intersection)
+        # p1=set(peers[box1])
+        # p2=set(peers[box2])
+        pint = set(peers[box1]) & set(peers[box2])
+        for p in pint:
+            i=values[p]
+            if len(i) > 2: 
+                for val in values[box1]:
+                    values[p] = values[p].replace(val,'')
+
+    return values
+
+def grid2values(grid):
+    values = []
+    all_digits = '123456789'
+    for c in grid:
+        if c == '.':
+            values.append(all_digits)
+        elif c in all_digits:
+            values.append(c)
+    assert len(values) == 81
+    return dict(zip(boxes, values))
+
+
+def display(values):
+    m = max(len(values[s]) for s in boxes)
+    width = 1 + m
+    line = '+'.join(['-'*(width*3)]*3)
+    for r in rows:
+        print(''.join(values[r+c].center(width)+('|' if c in '36' else '')
+                      for c in cols))
+        if r in 'CF': print(line)
+    print
 
 def eliminate(values):
-    """Apply the eliminate strategy to a Sudoku puzzle
+    
+    solved_values = [box for box in values.keys() if len(values[box]) == 1]
 
-    The eliminate strategy says that if a box has a value assigned, then none
-    of the peers of that box can have the same value.
-
-    Parameters
-    ----------
-    values(dict)
-        a dictionary of the form {'box_name': '123456789', ...}
-
-    Returns
-    -------
-    dict
-        The values dictionary with the assigned values eliminated from peers
-    """
-    # TODO: Copy your code from the classroom to complete this function
-    raise NotImplementedError
-
+    for solved_val in solved_values:
+        digit = values[solved_val]
+        peers_solv = peers[solved_val]
+        for peer in peers_solv:
+            values[peer] = values[peer].replace(digit,'')
+    return values
 
 def only_choice(values):
-    """Apply the only choice strategy to a Sudoku puzzle
+    
+    for unit in unitlist:
+        for digit in '123456789':
+            dplaces = [box for box in unit if digit in values[box]]
+            if len(dplaces) == 1:
+                values[dplaces[0]] = digit
+    return values
 
-    The only choice strategy says that if only one box in a unit allows a certain
-    digit, then that box must be assigned that digit.
-
-    Parameters
-    ----------
-    values(dict)
-        a dictionary of the form {'box_name': '123456789', ...}
-
-    Returns
-    -------
-    dict
-        The values dictionary with all single-valued boxes assigned
-
-    Notes
-    -----
-    You should be able to complete this function by copying your code from the classroom
-    """
-    # TODO: Copy your code from the classroom to complete this function
-    raise NotImplementedError
 
 
 def reduce_puzzle(values):
-    """Reduce a Sudoku puzzle by repeatedly applying all constraint strategies
+    
+    solved_values = [box for box in values.keys() if len(values[box]) == 1]
+    stalled = False
+    while not stalled:
+        solved_values_before = len([box for box in values.keys() if len(values[box]) == 1])
+        values = eliminate(values)
 
-    Parameters
-    ----------
-    values(dict)
-        a dictionary of the form {'box_name': '123456789', ...}
+        values = only_choice(values)
+        
+        values = naked_twins(values)
 
-    Returns
-    -------
-    dict or False
-        The values dictionary after continued application of the constraint strategies
-        no longer produces any changes, or False if the puzzle is unsolvable 
-    """
-    # TODO: Copy your code from the classroom and modify it to complete this function
-    raise NotImplementedError
+
+        solved_values_after = len([box for box in values.keys() if len(values[box]) == 1])
+        stalled = solved_values_before == solved_values_after
+        if len([box for box in values.keys() if len(values[box]) == 0]):
+            return False
+    
+    return values
 
 
 def search(values):
-    """Apply depth first search to solve Sudoku puzzles in order to solve puzzles
-    that cannot be solved by repeated reduction alone.
+    values = reduce_puzzle(values)
+    if values == False:
+        return False
 
-    Parameters
-    ----------
-    values(dict)
-        a dictionary of the form {'box_name': '123456789', ...}
+    if all(len(values[s]) == 1 for s in boxes):
+        return values 
 
-    Returns
-    -------
-    dict or False
-        The values dictionary with all boxes assigned or False
-
-    Notes
-    -----
-    You should be able to complete this function by copying your code from the classroom
-    and extending it to call the naked twins strategy.
-    """
-    # TODO: Copy your code from the classroom to complete this function
-    raise NotImplementedError
+    n,s = min((len(values[s]), s) for s in boxes if len(values[s]) > 1)
+    for value in values[s]:
+        new_sudoku = values.copy()
+        new_sudoku[s] = value
+        attempt = search(new_sudoku)
+        if attempt:
+            return attempt
 
 
 def solve(grid):
     """Find the solution to a Sudoku puzzle using search and constraint propagation
-
     Parameters
     ----------
     grid(string)
         a string representing a sudoku grid.
         
         Ex. '2.............62....1....7...6..8...3...9...7...6..4...4....8....52.............3'
-
     Returns
     -------
     dict or False
